764df84fc3b7616509452457afc14adf1e83351a
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-12 23:24:05 +0530

    Addes notes of List,Set and Tuple

---
+++ collection.py
@@ -0,0 +1,18 @@
+#Collection = single "variable" that can store multiple values!
+
+#LIST:
+fruits=["apple","banana","strawberry"] #Ordered and changebale and duplicates are allowed!
+print(fruits) #Indexing can be used!
+#To add an element we can use the append method and to remove an element we can use remove method!
+#To add an elemnt to a speicif index we can use the insert method!
+#To reverse the list we can use the reverse method and to sort the list we can use the sort method!
+
+#SET:
+fruits={"apple","orange","banana","coconut"} #Unordered and immutable , can add/remove , duplicates not allowed!
+#Cant use indexing as they are unordered!!
+#To check if an element is there in the set or not we can use the in method!
+
+#TUPLE:
+fruits=("apple","orange","banana","coconut") #Ordered and unchangable , FASTER , duplicates are allowed!
+#To check if an element is there in the tuple or not we can use the in method!
+#To check the index of an element we can use the index method!
