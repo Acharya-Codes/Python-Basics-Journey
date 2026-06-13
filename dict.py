3b9d393723c77431171f8ffdd6fac7111478b3c4
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-13 18:24:15 +0530

    Modified

--- dict.py
+++ dict.py
@@ -2,10 +2,10 @@
 capitals={"USA":"Washington DC",
 "India":"New Delhi"
 ,"Russia":"Moscow",
-"China:Beijing"}
+"China":"Beijing"}
 
 print(capitals.get("USA")) # The value assosiated with the key USA will be printed!
-print(capitals.het("Japan")) # There is no key named Japan so the output will be "none"!
+print(capitals.get("Japan")) # There is no key named Japan so the output will be "none"!
 
 capitals.update({"Germany":"Berlin"}) # Basically to add a key-value pair to the dictionary!
 # We can update a value for the key too!
