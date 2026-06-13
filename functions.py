6d5528888d7e27ca7dd75e74e954bcfc76f12d93
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-14 00:23:00 +0530

    Added functions

---
+++ functions.py
@@ -0,0 +1,5 @@
+def mario(name,age):  # Position of the parameters matters!
+    print("Hello people")
+    print(f"Its a me....{name}") # Inspiered from the meme "its a me....MARIOOO"
+    print(f"I am {age} years old")
+mario("ACHARYA",17)
