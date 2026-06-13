085565d13f5edd43e69ef1f19989cfad223df07a
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-13 18:22:38 +0530

    Added basic dictionary notes

---
+++ dict.py
@@ -0,0 +1,16 @@
+#Dictornaries = collection of {key:value} pairs , NO DUPLICATIONS!
+capitals={"USA":"Washington DC",
+"India":"New Delhi"
+,"Russia":"Moscow",
+"China:Beijing"}
+
+print(capitals.get("USA")) # The value assosiated with the key USA will be printed!
+print(capitals.het("Japan")) # There is no key named Japan so the output will be "none"!
+
+capitals.update({"Germany":"Berlin"}) # Basically to add a key-value pair to the dictionary!
+# We can update a value for the key too!
+# We can use the pop() method to remove a key-value pair!
+# popitem() will remove the latest key-value pair updated or inserted!
+
+# The keys() method can be used to get all the keys in a dictionary!
+# The values() method can be used to get all the values in a dictionary!
