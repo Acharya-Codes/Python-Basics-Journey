4a89e851b14b1ca13d6e0c926f8e95055aa2d7b9
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-15 01:08:10 +0530

    Added a basic working bank

---
+++ q21.py
@@ -0,0 +1,52 @@
+balance=0
+is_running=True
+
+def show_balance():
+    print(f"Your balance is: {balance}")
+    
+def deposit():
+    global is_running
+    global balance # As balance is a global variable we have to use the gloabal keyword
+    dep = int(input("Enter the non negative amount u wanna deposit: "))
+    balance += dep  
+    ch1 = input("Do you want to check ur balance?(Yes/No): ").capitalize()
+    if ch1 == "Yes":
+        print(f"Your balance is: {balance}")
+    else:
+        is_running=False
+        print("---Thank you for visiting us!---")
+
+def withdraw():
+    global is_running
+    global balance # As balance is a global variable we have to use the gloabal keyword
+    wit = int(input("Enter the non negative amount u wanna withdraw: "))
+    balance -= wit  
+    wit1 = input("Do you want to check ur balance?(Yes/No): ").capitalize()
+    if wit1 == "Yes":
+        print(f"Your balance is: {balance}")
+    else:
+        is_running=False
+        print("---Thank you for visiting us!---")
+
+
+while is_running:
+    print("Welcome to Aachi's Bank")
+    print("1.Show Balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = input("Choose your action (1 to 4): ")
+
+    if choice == "1":
+        show_balance()
+    elif choice == "2":
+        deposit()
+    elif choice == "3":
+        withdraw()
+    elif choice == "4":
+        is_running=False
+        print("---Thank you for visiting us!---")
+    else:
+        print("That is not a valid choice")
+        
