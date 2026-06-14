11f920798c3432feec98adb5e2c6c82e8d019aa1
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-14 23:50:29 +0530

    Added this subfile to understand creation of own modules

---
+++ ownmodule.py
@@ -0,0 +1,11 @@
+import ownmod
+result = print(ownmod.square(2))
+result = print(ownmod.cube(3))
+
+# Code of ownmod.py:
+
+#def square(x):
+    #return x**2
+#def cube(x):
+   # return x**3
+# ------------------
