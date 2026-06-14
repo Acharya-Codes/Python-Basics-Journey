7d15c62a8baa2075cf97cfde6eec23d6b0957520
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-14 14:00:01 +0530

    Added iterables

---
+++ iterables.py
@@ -0,0 +1,13 @@
+numbers=[1,2,3,4,5]
+for num in numbers:
+    print(num,end=" ")
+fruits={"apple","orange","banana","coconut"}
+for fruit in fruits:
+    print(fruit,end=" ")
+name = "Acharya"
+for char in name:
+    print(char,end=" ")
+dict={"A":1,"B":2}
+for key,value in dict.items():
+    print(f"{key}:{value}",end=" ")
+
