5941444a34ad7d2936b5cf08961afeca520bea4d
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-15 00:27:16 +0530

    Added this thingy: if __name__ = __main__

---
+++ main_check.py
@@ -0,0 +1,9 @@
+# if__name__ == "__main__"  (This can be imported or used standalone)
+
+print("This line always runs!")
+
+if __name__ == "__main__":
+    print("This code was run directly")
+else:
+    print("This code was imported")
+
---
+++ test_check.py
@@ -0,0 +1,3 @@
+import main_check
+print("Test file done")
+# Bascially we imported the main_check file so the __name__ doesnt match with __main__ !
