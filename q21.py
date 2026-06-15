de99dc14d0fa0e74f632e828d5fe5b35f17fb6b4
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-15 18:53:29 +0530

    Added basic banking system

--- q21.py
+++ q21.py
@@ -1,10 +1,12 @@
 balance=0
+
 is_running=True
 
 def show_balance():
     print(f"Your balance is: {balance}")
     
 def deposit():
+    
     global is_running
     global balance # As balance is a global variable we have to use the gloabal keyword
     dep = int(input("Enter the non negative amount u wanna deposit: "))
@@ -17,6 +19,7 @@ def deposit():
         print("---Thank you for visiting us!---")
 
 def withdraw():
+    
     global is_running
     global balance # As balance is a global variable we have to use the gloabal keyword
     wit = int(input("Enter the non negative amount u wanna withdraw: "))
@@ -31,22 +34,36 @@ def withdraw():
 
 while is_running:
     print("Welcome to Aachi's Bank")
-    print("1.Show Balance")
-    print("2.Deposit")
-    print("3.Withdraw")
-    print("4.Exit")
-
-    choice = input("Choose your action (1 to 4): ")
-
-    if choice == "1":
-        show_balance()
-    elif choice == "2":
-        deposit()
-    elif choice == "3":
-        withdraw()
-    elif choice == "4":
-        is_running=False
-        print("---Thank you for visiting us!---")
+    print("1.Register a new account")
+    print("2.Login")
+    req = int(input("Select your action(1 or 2): "))
+    
+    if req == 1:
+        
+        a = int(input("Enter a non negative amount to add to your balance: "))
+        balance = a
+        print(f"Your balance has been updated to: {a}")
     else:
-        print("That is not a valid choice")
+        print("Welcome to Aachi's Bank")
+        print("1.Show Balance")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
+
+        choice = input("Choose your action (1 to 4): ")
+
+        if choice == "1":
+            show_balance()
+        elif choice == "2":
+            deposit()
+        elif choice == "3":
+            withdraw()
+        elif choice == "4":
+            is_running=False
+            print("---Thank you for visiting us!---")
+        else:
+            print("That is not a valid choice")
+
+
+    
         
