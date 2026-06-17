b220e308e4dabda73186a33f01700e0efd8e6201
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-17 16:16:13 +0530

    Added OOP

---
+++ OOP.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self,model,name,colour,for_sale):  #This method is ued to create functions
+        self.model = model
+        self.name = name
+        self.colour = colour
+        self.for_sale = for_sale
+
+Car1 = Car(2,"Mustang","Red",True)
+Car2 = Car(1,"Waganor","Blue",False)  
+
