c459c724450a611153b5c9ab431a70687ad76a3f
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-18 15:52:15 +0530

    Added File Writing in .txt format

--- Files/pythonwriting.py
+++ Files/pythonwriting.py
@@ -1,5 +1,6 @@
-txt_data = "I Love You"
+employees = ["Acharya","Kavi","Siddhesh"]
 file_path = "output.txt"
 with open(file_path,"w") as file2: # w for write , x also to write but used when the text file doesnt exist!
-    file2.write(txt_data)
-    print(f"{file_path} as a text file was created")
+    for employee in employees:
+        file2.write(employee + "\n")
+        print(f"{file_path} as a text file was created")
