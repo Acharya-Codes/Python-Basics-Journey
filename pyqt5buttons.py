db866410dda440fba538c1a8111846cb67ecc3df
Acharya-Codes <acharyan7a1@gmail.com>
2026-06-19 12:54:55 +0530

    Added Buttons in PyQt5

--- pyqt5buttons.py
+++ pyqt5buttons.py
@@ -5,20 +5,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(0,0,500,500)
+        self.button = QPushButton("Click this to get free 1000 diamonds💎 in FF😏!",self)
+        self.label = QLabel("Hello Surviver",self)
         self.initUI()
 
     def initUI(self):
-        self.button = QPushButton("Click this to get free 1000 diamonds💎 in FF😏!",self)
         self.button.setGeometry(25,200,450,100)
         self.button.setStyleSheet("font-size:20px;")
-        
         self.button.clicked.connect(self.on_click)  # Connect the on_click function and the button!
+        self.label.setGeometry(10,100,400,100)
+        self.label.setStyleSheet("font-size:50px;")
 
     def on_click(self):
+        self.label.setText("Byebye")
         print("Congrats u got scammed NOOB 😂")
         self.button.setText("NOOB😂")
-
-
+        self.button.setDisabled(True)
 
 def main():
     app = QApplication(sys.argv)
